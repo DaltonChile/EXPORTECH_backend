b6a0dbdf773ad7f81fbff16b6ea68c4786bbcaec
ftorowest <ftorow@uc.cl>
2026-01-28 15:42:23 -0300

    Use env vars for email config

--- exportech_backend/settings.py
+++ exportech_backend/settings.py
@@ -162,9 +162,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'exportech088@gmail.com'
-EMAIL_HOST_PASSWORD = 'beflsrhaxqhnpjwh'
-DEFAULT_FROM_EMAIL = 'Exportech <exportech088@gmail.com>'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'exportech088@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'beflsrhaxqhnpjwh')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'Exportech <exportech088@gmail.com>')
 
 # Para desarrollo (muestra emails en consola):
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
